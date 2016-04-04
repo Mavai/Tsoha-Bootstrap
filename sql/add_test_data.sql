90eed6db27acbb9423ea5d1bad81bd14b920beea
Mavai <marko.vainio14@gmail.com>
2016-04-04 18:55:19 +0300

    tietokantataulut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,13 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Kurssi (nimi) VALUES ('Ohjelmoinnin harjoitustyö');
+
+INSERT INTO Opettaja (nimi) VALUES ('Matti Meikäläinen');
+
+INSERT INTO Opiskelija (opiskelijanumero, nimi) VALUES (0123456789, 'Marko Vainio');
+
+INSERT INTO Aihe (nimi, vaikeusaste, maksimiarvosana, kuvaus) VALUES 
+            ('Miinaharava', 'Helppo', 5, 'Perinteinen miinaharava- peli');
+
+INSERT INTO Harjoitustyö (alkamispvm, päättymispvm, arvosana) VALUES 
+            ('2016-01-01', '2016-03-04', 5)
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kurssi(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50)
+);
+
+CREATE TABLE Opettaja(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50)
+);
+
+CREATE TABLE Opiskelija(
+    opiskelijanumero NUMERIC PRIMARY KEY,
+    nimi varchar(50)
+);
+
+CREATE TABLE Aihe(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50),
+    vaikeusaste varchar(50),
+    maksimiarvosana INTEGER,
+    kuvaus varchar(500)
+);
+
+CREATE TABLE Harjoitustyö(
+    id SERIAL PRIMARY KEY,
+    alkamispvm DATE,
+    päättymispvm DATE,
+    arvosana INTEGER,
+    kurssi INTEGER REFERENCES Kurssi(id),
+    opettaja INTEGER REFERENCES Opettaja(id),
+    opiskelija INTEGER REFERENCES Opiskelija(opiskelijanumero),
+    aihe INTEGER REFERENCES Aihe(id)
+);
